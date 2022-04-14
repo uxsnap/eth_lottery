256ef97085000f7a583935ee09a0bde1efc303cc
nuxxxcake <nuxxx7132@gmail.com>
2022-04-14 18:13:43 +0300

    NuxToken added

--- client/src/pages/Converter/index.tsx
+++ client/src/pages/Converter/index.tsx
@@ -1,7 +1,10 @@
-import React, { useState } from 'react';
+import { ethers } from "ethers";
+import { observer } from "mobx-react"
+import React, { useEffect, useState } from 'react';
 
 import { Button } from '../../components';
 import { InputSelect } from '../../components/InputSelect';
+import { web3Store } from "../../mobxStore";
 import { handleNumbers } from '../../utils';
 
 import styles from './Converter.module.scss';
@@ -12,17 +15,66 @@ const currencyOptions = [
   { label: 'CUR', value: 'cur' }
 ];
 
-export const Converter = () => {
-  const [from, setFrom] = useState('');
-  const [to, setTo] = useState('');
+export const Converter = observer(() => {
+  const [buy, setBuy] = useState('');
+  const [sell, setSell] = useState('');
+  const [buyCur, setBuyCur] = useState('');
+  const [sellCur, setSellCur] = useState('');
+  const [curAmount, setCurAmount] = useState('');
 
+  const { erc20, signer } = web3Store;
+
+  const getBalance = async () => {
+    const address = await signer.getAddress();
+    // @ts-ignore
+    const amount = await erc20.balanceOf(address);
+    setCurAmount(amount.toString());
+  };
+
+  useEffect(() => {
+    getBalance();
+  });
+
+  const handleClick = async () => {
+    let amount = await ethers.utils.parseEther(buy.toString());
+
+    console.log(buy, amount);
+
+    // @ts-ignore
+    await erc20.buyToken(buy, { value: amount });
+    getBalance();
+  };
 
   return (
-    <form className={styles.Root} action="POST">
-      <InputSelect mask={handleNumbers} value={from} onChange={(e: any) => setFrom(e.target.value)} label="From" options={currencyOptions} />
-      <InputSelect mask={handleNumbers} value={to} onChange={(e: any) => setTo(e.target.value)} label="To" options={currencyOptions} />
+    <div className={styles.Root}>
+      {curAmount}
+      <div>
+        <InputSelect
+          label="Buy: "
+          mask={handleNumbers}
+          selectValue={buyCur}
+          onSelect={setBuyCur}
+          value={buy}
+          onChange={(e: any) => setBuy(e.target.value)}
+          options={currencyOptions}
+        />
 
-      <Button>Submit</Button>
-    </form>
+        <Button onClick={handleClick}>Buy</Button>
+      </div>
+
+      <div>
+        <InputSelect
+          label="Sell: "
+          mask={handleNumbers}
+          value={sell}
+          selectValue={sellCur}
+          onSelect={setSell}
+          onChange={(e: any) => setSellCur(e.target.value)}
+          options={currencyOptions}
+        />
+
+        <Button>Sell</Button>
+      </div>
+    </div>
   );
-}
+});
